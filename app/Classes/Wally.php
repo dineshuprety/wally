9a9b083464bd413a2162541bd238e5aa23b6b02f
dineshuprety <dineshuprety500@gmail.com>
2023-07-28 17:01:20 +0545

    working on console application

--- app/Classes/Wally.php
+++ app/Classes/Wally.php
@@ -2,7 +2,36 @@
 
 namespace App\Classes;
 
-class Wally
-{
+use function Termwind\ask;
 
+trait Wally
+{
+    protected $name;
+    protected $des;
+
+    public function khataName()
+    {
+        $this->name = $this->argument('name');
+        if (!$this->argument('name')) {
+            $this->name = ask(<<<HTML
+            <span class="mt-1 ml-2 mr-1 bg-red px-1 text-black">
+                Name of your khata:
+            </span>
+        HTML);
+        }
+        return $this->name;
+    }
+
+    public function khataDes()
+    {
+        $this->des = $this->argument('note');
+        if (!$this->argument('des')) {
+            $this->des = ask(<<<HTML
+            <span class="mt-1 ml-2 mr-1 bg-red px-1 text-black">
+                Add your description:
+            </span>
+        HTML);
+        }
+        return $this->des;
+    }
 }
--- app/Commands/Khata.php
+++ app/Commands/Khata.php
@@ -4,22 +4,27 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use App\Classes\Wally;
+use function Termwind\{render};
 
 class Khata extends Command
 {
+    use Wally;
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'khata';
+    protected $signature = 'khata {name? : khata name is requried}
+                            {--sn|note : Description about khata}
+    ';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'create the khata';
 
     /**
      * Execute the console command.
@@ -28,7 +33,9 @@ class Khata extends Command
      */
     public function handle()
     {
-        //
+        $name = $this->khataName();
+        $des = $this->khataDes();
+        $this->info("My khata was created with name: {$name} and description {$des}");
     }
 
     /**
